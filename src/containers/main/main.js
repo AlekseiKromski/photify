a3077771462c43f26705b1ac36a58f789eeb1cbe
AlekseiKromski <aleksei.kromski@outlook.com>
2022-05-05 20:39:48 +0300

    rework navigation with new design

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-
-
+import Classes from './main.module.css'
+import Profile from '../../components/main/profile/profile'
 class Main extends Component{
     render(){
         return (
-            <p>main</p>
+            <div className={'row ' + Classes.main}>
+                <div className={Classes.posts}>
+                    <Profile></Profile>
+                </div>
+            </div>
         )
     }
 }
