4b4f7f5815de671ca8543081fc08317fc002b4f2
AlekseiKromski <aleksei.kromski@outlook.com>
2022-05-19 15:38:55 +0300

    create profile page

--- src/components/profile/mainProfile/mainProfile.js
+++ src/components/profile/mainProfile/mainProfile.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
 import Classes from './mainProfile.module.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUserCheck, faUserPlus} from '@fortawesome/free-solid-svg-icons'
+import {faUserCheck, faUserPlus, faPencil} from '@fortawesome/free-solid-svg-icons'
 import {faInstagram} from '@fortawesome/free-brands-svg-icons'
 class MainProfile extends Component {
     render() {
         return (
             <div className={Classes.profile}>
+                <FontAwesomeIcon className={Classes.edit} icon={faPencil}></FontAwesomeIcon>
                 <div className={Classes.profileImage} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
                 <h1>#milana</h1>
                 <p>Proffesional designer</p>
                 <div className={Classes.stat}>
                     <div className={Classes.statBlock}>
-                        <FontAwesomeIcon icon={faUserCheck}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={Classes.icon} icon={faUserCheck}></FontAwesomeIcon>
                         <span>43</span>
                     </div>
                     <div className={Classes.statBlock}>
-                        <FontAwesomeIcon icon={faUserPlus}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={Classes.icon} icon={faUserPlus}></FontAwesomeIcon>
                         <span>43</span>
                     </div>
                     <div className={Classes.statBlock}>
-                        <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={Classes.icon} icon={faInstagram}></FontAwesomeIcon>
                         <span>43</span>
                     </div>
                 </div>
--- src/components/profile/posts/Posts.js
+++ src/components/profile/posts/Posts.js
@@ -4,7 +4,27 @@ class Posts extends Component {
     render() {
         return (
             <div className={Classes.posts}>
-                ttest
+                <h1>Posts</h1>
+                <div className={Classes.postRow}>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                </div>
+                <div className={Classes.postRow}>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                </div>
+                <div className={Classes.postRow}>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                </div>
+                <div className={Classes.postRow}>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                    <div className={Classes.post} style={{background: "url('/img/profiles/aiony-haust-3TLl_97HNJo-unsplash.jpg')"}}></div>
+                </div>
             </div>
         );
     }
