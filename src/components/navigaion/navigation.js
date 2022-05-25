85e1354d0a7db00acc7bd0f916e005399e5784cb
alekseiKromski <aleksei.kromski@outlook.com>
2022-05-26 00:06:18 +0300

    add active buttons

--- src/components/navigaion/navigation.js
+++ src/components/navigaion/navigation.js
@@ -1,8 +1,16 @@
 import React, {Component} from "react";
 import Classes from './navigation.module.css';
-import {Link} from 'react-router-dom'
+import {NavLink, Link} from 'react-router-dom'
 class Navigation extends Component{
 
+    defineStyles({isActive}){
+        let classes = []
+        if(isActive){
+            classes.push(Classes.activeMenu)
+        }
+        classes.push(Classes.iconBlock)
+        return classes.join(' ')
+    }
     render() {
 
         return (
@@ -12,12 +20,9 @@ class Navigation extends Component{
                 <h1 className={Classes.logo}><Link to={'/'}>Photify</Link></h1>
                 <input className={Classes.search} placeholder={'Find account'}/>
                 <div className={Classes.iconBlocks}>
-                    <div className={Classes.iconBlock}>
-                        {/*<div className={Classes.notification}></div>*/}
-                        <Link to={'/chat'}>
-                            <svg className={Classes.icon} xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M24,16v5a3,3,0,0,1-3,3H16a8,8,0,0,1-6.92-4,10.968,10.968,0,0,0,2.242-.248A5.988,5.988,0,0,0,16,22h5a1,1,0,0,0,1-1V16a5.988,5.988,0,0,0-2.252-4.678A10.968,10.968,0,0,0,20,9.08,8,8,0,0,1,24,16ZM17.977,9.651A9,9,0,0,0,8.349.023,9.418,9.418,0,0,0,0,9.294v5.04C0,16.866,1.507,18,3,18H8.7A9.419,9.419,0,0,0,17.977,9.651Zm-4.027-5.6a7.018,7.018,0,0,1,2.032,5.46A7.364,7.364,0,0,1,8.7,16H3c-.928,0-1-1.275-1-1.666V9.294A7.362,7.362,0,0,1,8.49,2.018Q8.739,2,8.988,2A7.012,7.012,0,0,1,13.95,4.051Z"/></svg>
-                        </Link>
-                    </div>
+                    <NavLink className={this.defineStyles} to={'/chat'}>
+                        <svg className={Classes.icon} xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M24,16v5a3,3,0,0,1-3,3H16a8,8,0,0,1-6.92-4,10.968,10.968,0,0,0,2.242-.248A5.988,5.988,0,0,0,16,22h5a1,1,0,0,0,1-1V16a5.988,5.988,0,0,0-2.252-4.678A10.968,10.968,0,0,0,20,9.08,8,8,0,0,1,24,16ZM17.977,9.651A9,9,0,0,0,8.349.023,9.418,9.418,0,0,0,0,9.294v5.04C0,16.866,1.507,18,3,18H8.7A9.419,9.419,0,0,0,17.977,9.651Zm-4.027-5.6a7.018,7.018,0,0,1,2.032,5.46A7.364,7.364,0,0,1,8.7,16H3c-.928,0-1-1.275-1-1.666V9.294A7.362,7.362,0,0,1,8.49,2.018Q8.739,2,8.988,2A7.012,7.012,0,0,1,13.95,4.051Z"/></svg>
+                    </NavLink>
                     <div onClick={() => {
                         this.props.showEventsTrigger();
                     }}
@@ -28,18 +33,12 @@ class Navigation extends Component{
                         {/*<div className={Classes.notification}></div>*/}
                         <svg className={Classes.icon} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"/></svg>
                     </div>
-                    <div className={Classes.iconBlock}>
-                        {/*<div className={Classes.notification}></div>*/}
-                        <Link to={'/profile'}>
-                            <svg className={Classes.icon} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/></svg>
-                        </Link>
-                    </div>
-                    <div className={Classes.iconBlock}>
-                        {/*<div className={Classes.notification}></div>*/}
-                        <Link to={'/create-post'}>
-                            <svg className={Classes.icon} id="Layer_1" height="512" viewBox="0 0 24 24" width="512" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="m12 0a12 12 0 1 0 12 12 12.013 12.013 0 0 0 -12-12zm0 22a10 10 0 1 1 10-10 10.011 10.011 0 0 1 -10 10zm5-10a1 1 0 0 1 -1 1h-3v3a1 1 0 0 1 -2 0v-3h-3a1 1 0 0 1 0-2h3v-3a1 1 0 0 1 2 0v3h3a1 1 0 0 1 1 1z"/></svg>
-                        </Link>
-                    </div>
+                    <NavLink className={this.defineStyles} to={'/profile'}>
+                        <svg className={Classes.icon} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/></svg>
+                    </NavLink>
+                    <NavLink className={this.defineStyles} to={'/create-post'}>
+                        <svg className={Classes.icon} id="Layer_1" height="512" viewBox="0 0 24 24" width="512" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="m12 0a12 12 0 1 0 12 12 12.013 12.013 0 0 0 -12-12zm0 22a10 10 0 1 1 10-10 10.011 10.011 0 0 1 -10 10zm5-10a1 1 0 0 1 -1 1h-3v3a1 1 0 0 1 -2 0v-3h-3a1 1 0 0 1 0-2h3v-3a1 1 0 0 1 2 0v3h3a1 1 0 0 1 1 1z"/></svg>
+                    </NavLink>
                 </div>
             </div>
         )
